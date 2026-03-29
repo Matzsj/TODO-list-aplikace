6563289fbc480e182309d038f10c3a9cf1770d54
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-03-29 11:08:19 +0200

    oprava ukladani save jako ukol

--- TODO/Program.cs
+++ TODO/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace TODO
+﻿using System;
+using System.Collections.Generic;
+using System.IO; // <-- Nezapomeň přidat toto!
+
+namespace TODO
 {
     internal class Program
     {
@@ -8,6 +12,13 @@
         private static string ukol = null;
         private static void Main(string[] args)
         {
+            if (File.Exists("ukoly.txt"))
+            {
+                string[] nacteneUkoly = File.ReadAllLines("ukoly.txt");
+
+                ukoly.AddRange(nacteneUkoly);
+            }
+
             Console.WriteLine("Vitej v TODO aplikaci");
             Console.WriteLine("-------------------------------------------");
             Console.WriteLine("");
@@ -16,7 +27,7 @@
 
                 Console.Write("Co je vas ukol: ");
                 string ukol = Console.ReadLine();
-                if (ukol != "show" && ukol != "end" && ukol != "clear" && ukol != "help" && !ukol.StartsWith("delete "))
+                if (ukol != "show" && ukol != "end" && ukol != "clear" && ukol != "help" && !ukol.StartsWith("delete ") && ukol != "save")
                 {
                     ukoly.Add(ukol);
                 }
@@ -55,6 +66,8 @@
                     Console.WriteLine("show - ukaze vsechny ukoly v listu");
                     Console.WriteLine("end - ukonci aplikaci");
                     Console.WriteLine("clear - vymaze vsechny ukoly v listu");
+                    Console.WriteLine("save - ulozi vsechny vase ukoly (kdyz vypnete aplikaci tak se ulozi samy)");
+                    Console.WriteLine("delete + {nazev_ukolu} - vymaze ukol ktery mate vytvoreny)");
                     Console.WriteLine("---------------------");
                     Console.WriteLine("");
                 }
@@ -69,11 +82,17 @@
                         Console.WriteLine($"Ukol '{smazani}' byl smazan.");
                         Console.WriteLine("---------------------");
                         Console.WriteLine("");
-
-
                     }
                 }
+                if (ukol == "save")
+                {
+                    File.WriteAllLines("ukoly.txt", ukoly); 
+                }
+
             }
+            
+            File.WriteAllLines("ukoly.txt", ukoly);
+            Console.WriteLine("Vsechny ukoly byly ulozeny.");
 
         }
     }
